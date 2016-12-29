b3c779a99f45a1d423db6c96be4fcd62a5273117
deepak arya <deepak arya>
2016-12-29 07:24:27 -0700

    Second Commit

---
+++ src/test/java/seleniumPracticeMaven/sampleTest.java
@@ -0,0 +1,14 @@
+package seleniumPracticeMaven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by deepak on 12/29/2016.
+ */
+public class sampleTest {
+
+@Test
+    public void printMessage(){
+    System.out.println("print the message");
+}
+}
